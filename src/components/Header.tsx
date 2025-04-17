afc0cf00e276c4ac4faee002904e05791fd92967
Isuru Dissanayake <isuru098@gmail.com>
2025-04-17 08:55:11 +0530

    NavBar Styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,26 @@
 import React from "react";
 import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@heroui/react";
+import "./ComponentStyles.css"
 
 const Header: React.FC = () => {
   return (
-      <Navbar isBlurred={true} shouldHideOnScroll position="sticky" className="fixed top-0 left-0 z-50 text-[1.25em] p-4 w-full bg-white/30 backdrop-blur-sm">
-        <NavbarContent className="hidden sm:flex" justify="start">
-          <NavbarItem>
-            <span className="material-symbols-rounded">home</span>
+      <Navbar isBlurred={true} shouldHideOnScroll position="sticky" className="fixed top-0 left-0 z-50 text-[1.4em] p-7 w-full bg-white/30 backdrop-blur-sm">
+        <NavbarContent className="hidden sm:flex space-x-8" justify="start">
+          <NavbarItem className="whitespace-nowrap">
+              <span className="material-symbols-rounded hover:text-white transform transition-all duration-200 hover:cursor-pointer" style={{ fontSize: "2rem" }}>home</span>
           </NavbarItem>
 
-          <NavbarItem>
+          <NavbarItem className="whitespace-nowrap hover:text-white transform transition-all duration-200">
             <Link color="foreground" href="#">
               Experience
             </Link>
           </NavbarItem>
 
-          <NavbarItem isActive>
+          <NavbarItem isActive className="whitespace-nowrap hover:text-white transform transition-all duration-200">
             <Link aria-current="page" href="#">
               Explore
             </Link>
           </NavbarItem>
-          
         </NavbarContent>
 
         <NavbarContent className="flex justify-center w-full" justify="center">
@@ -29,14 +29,14 @@ const Header: React.FC = () => {
           </NavbarBrand>
         </NavbarContent>
 
-        <NavbarContent justify="end">
-          <NavbarItem>
+        <NavbarContent className="flex space-x-8" justify="end">
+          <NavbarItem className="whitespace-nowrap hover:text-white transform transition-all duration-200">
             <Link color="foreground" href="#">
               Start Safari
             </Link>
           </NavbarItem>
 
-          <NavbarItem>
+          <NavbarItem className="whitespace-nowrap hover:text-white transform transition-all duration-200">
             <Button as={Link} color="primary" href="#" variant="flat">
               Enquire Now
             </Button>
