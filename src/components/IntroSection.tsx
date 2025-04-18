ea3406ea0111a46b9fa4f6acf3727c6567960f90
Isuru Dissanayake <56559893+IsuruPD@users.noreply.github.com>
2025-04-18 20:51:06 +0530

    Update Layout Config in IntroSection

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -4,19 +4,24 @@ import "./ComponentStyles.css"
 const IntroSection: React.FC = () => {
   return (
     <section className="introSection">
-      <img src="treeVector.png" alt="Safari icon" className="introSectionIcon" />
-      <div className="introDesc">
-        <span className="introHeading">
-          <span className="hotelName">DSP</span> is a wildlife-rich
-          <br />
-          private reserve in Wipattuwa
-          <br />
-           area with 4 luxury lodges,
-          <br/>
-        </span>
-        <span className="introSubheading"> each with its own</span>
-        <span className="introSubheadingSecond"> character and style</span>
+
+      <div className="introCentered">
+        <img src="treeVector.png" alt="Safari icon" className="introSectionIcon" />
+
+        <div className="introDesc">
+          <span className="introDescP1">
+            <span className="hotelName">DSP</span> is a wildlife-rich
+            <br />
+            private reserve in Wilpattuwa
+            <br />
+            area with 4 luxury lodges,
+            <br/>
+          </span>
+          <span className="introDescP2"> each with its own</span>
+          <span className="introDescP3"> character and style</span>
+        </div>
       </div>
+
       
       <img src="monkeyVector.png" alt="Lodge image" className="lodgeImage" />
     </section>
