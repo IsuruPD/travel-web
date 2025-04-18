3fc407e2bd04440e97e4f2895fbe81d4cd02c89d
Isuru Dissanayake <isuru098@gmail.com>
2025-04-18 14:28:03 +0530

    Experience Section Responsiveness vf

--- src/components/ExperienceSection.tsx
+++ src/components/ExperienceSection.tsx
@@ -2,35 +2,35 @@ import React from "react";
 
 const ExperienceSection: React.FC = () => {
   return (
-    <section className="experienceSection flex ">
-
+    <section className="experienceSection">
       <div className="flex-1">
-        <img src="bird43.png" alt="Safari experience" className="experienceImage"
-      />
+        <img
+          src="bird43.png"
+          alt="Safari experience"
+          className="experienceImage"
+        />
       </div>
-      <div className="experienceTextContainer flex-1">
+
+      <div className="experienceTextSec flex-1">
+        <div className="experienceTextContainer">
           <div className="subSectionTitle">Where Luxury Meets Wilderness</div>
-          <br />
-          <div className="subSectionDescription flex-1">
+          <div className="subSectionDescription">
             Nestled on the edge of <span className="subSectionDescriptionEmp">Udawalawa,</span> our eco-lodges offer the perfect blend of comfort and raw nature. Wake up to the call of exotic birds, and sleep under a sky full of stars.
           </div>
 
-
-          <div className="">
-            <div className="container">
-              <a href="#" className="runningButton type-A">
-                <div className="runningButton__line"></div>
-                <div className="runningButton__line"></div>
-                <span className="runningButton__text">See more..</span>
-                <div className="runningButton__drow1"></div>
-                <div className="runningButton__drow2"></div>
-              </a>
-            </div>
-
+          <div className="container">
+            <a href="#" className="runningButton type-A">
+              <div className="runningButton__line"></div>
+              <div className="runningButton__line"></div>
+              <span className="runningButton__text">See more..</span>
+              <div className="runningButton__drow1"></div>
+              <div className="runningButton__drow2"></div>
+            </a>
           </div>
+        </div>
       </div>
-
     </section>
+
   );
 };
 
